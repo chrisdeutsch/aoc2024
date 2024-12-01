5a52a9fefe2d8776092e493e05705dbf2fcf9d97
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2024-12-01 09:50:46 +0100

    Day 1 Part 2

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,6 +1,7 @@
 #include "parsing.h"
 #include <algorithm>
 #include <iostream>
+#include <map>
 
 int main(int argc, char *argv[]) {
     const auto input = parse_input<int, int>(std::cin);
@@ -13,14 +14,33 @@ int main(int argc, char *argv[]) {
     std::sort(first.begin(), first.end());
     std::sort(second.begin(), second.end());
 
-    int total_distance = 0;
+    std::size_t total_distance = 0;
     for (auto it1 = first.cbegin(), it2 = second.cbegin();
          it1 != first.cend() && it2 != second.cend(); it1++, it2++) {
-        const auto dist = std::abs(*it1 - *it2);
+        const std::size_t dist = std::abs(*it1 - *it2);
         total_distance += dist;
     }
 
     std::clog << "Part 1: " << total_distance << "\n";
+
+    std::map<int, std::size_t> counts;
+    for (auto item : second) {
+        counts[item] += 1;
+    }
+
+    for (auto [k, v] : counts) {
+        std::clog << k << " " << v << "\n";
+    }
+
+    std::size_t similarity = 0;
+    for (auto item : first) {
+        const auto it = counts.find(item);
+        if (it != counts.cend()) {
+            similarity += item * it->second;
+        }
+    }
+
+    std::clog << "Part 2: " << similarity << "\n";
 
     return 0;
 }
