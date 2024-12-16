56c1fa3bc3d3d6b6184581437e8e658fbf1ff847
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2024-12-16 14:07:50 +0100

    Day 8 part 2

--- src/day8.cpp
+++ src/day8.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <map>
 #include <set>
@@ -57,6 +58,33 @@ antinode get_antinode(const antenna &a, const antenna &b) {
     };
 }
 
+std::vector<antinode> get_antinodes_pt2(const antenna &a, const antenna &b,
+                                        std::function<bool(int, int)> f_valid_loc) {
+    if (a.frequency != b.frequency) {
+        throw std::runtime_error("called get_antinodes on antennas with different frequencies");
+    }
+
+    const auto delta_row = b.row - a.row;
+    const auto delta_col = b.col - a.col;
+
+    std::vector<antinode> antinodes;
+
+    int row = b.row;
+    int col = b.col;
+    while (f_valid_loc(row, col)) {
+        antinodes.push_back({
+            .row = row,
+            .col = col,
+            .frequency = b.frequency,
+        });
+
+        row += delta_row;
+        col += delta_col;
+    }
+
+    return antinodes;
+}
+
 int main() {
     const auto [antennas, num_rows, num_cols] = read_input(std::cin);
     const auto antennas_by_frequency = group_antenna_by_frequency(antennas);
@@ -65,7 +93,8 @@ int main() {
         return 0 <= row && row < num_rows && 0 <= col && col < num_cols;
     };
 
-    std::vector<antinode> antinodes;
+    std::set<std::pair<int, int>> antinode_locations_pt1;
+    std::set<std::pair<int, int>> antinode_locations_pt2;
     for (const auto &[frequency, antennas] : antennas_by_frequency) {
         // All antenna pairs
         for (const auto &a : antennas) {
@@ -73,20 +102,24 @@ int main() {
                 if (&a == &b) {
                     continue;
                 }
+
+                // Part 1
                 const auto antinode = get_antinode(a, b);
                 if (is_valid_location(antinode.row, antinode.col)) {
-                    antinodes.push_back(antinode);
+                    antinode_locations_pt1.emplace(antinode.row, antinode.col);
+                }
+
+                // Part 2
+                const auto antinodes = get_antinodes_pt2(a, b, is_valid_location);
+                for (const auto &an : antinodes) {
+                    antinode_locations_pt2.emplace(an.row, an.col);
                 }
             }
         }
     }
 
-    std::set<std::pair<int, int>> unique_antinode_locations;
-    for (const auto &antinode : antinodes) {
-        unique_antinode_locations.insert(std::make_pair(antinode.row, antinode.col));
-    }
-
-    std::clog << "Part 1: " << unique_antinode_locations.size() << "\n";
+    std::clog << "Part 1: " << antinode_locations_pt1.size() << "\n";
+    std::clog << "Part 2: " << antinode_locations_pt2.size() << "\n";
 
     return 0;
 }
