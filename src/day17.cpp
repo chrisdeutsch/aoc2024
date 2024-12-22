0187333fe2075ecf4fe9b64eb4247d0c9f0db55d
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2024-12-22 17:19:09 +0100

    WIP

--- src/day17.cpp
+++ src/day17.cpp
@@ -1,7 +1,9 @@
+#include <absl/strings/str_join.h>
 #include <absl/strings/str_split.h>
 #include <iostream>
 #include <print>
 #include <regex>
+#include <set>
 #include <vector>
 
 struct program_state {
@@ -10,6 +12,16 @@ struct program_state {
     std::size_t reg_c = 0;
     std::vector<uint8_t> program;
     std::size_t ip = 0;
+
+    bool operator<(const program_state &other) {
+        return std::tie(reg_a, reg_b, reg_c, ip) <
+               std::tie(other.reg_a, other.reg_b, other.reg_c, ip);
+    }
+
+    bool operator==(const program_state &other) {
+        return reg_a == other.reg_a && reg_b == other.reg_b && reg_c == other.reg_c &&
+               program == other.program && ip == other.ip;
+    }
 };
 
 program_state read_input(std::istream &is) {
@@ -51,7 +63,7 @@ enum class opcode : uint8_t {
 };
 
 std::optional<std::size_t> combo_operand_to_value(uint8_t operand, const program_state &state) {
-    if (0 <= operand && operand <= 3) {
+    if (operand <= 3) {
         return operand;
     } else if (operand == 4) {
         return state.reg_a;
@@ -63,7 +75,7 @@ std::optional<std::size_t> combo_operand_to_value(uint8_t operand, const program
     return {};
 }
 
-void execute_instruction(opcode oc, uint8_t operand, program_state &state) {
+std::optional<uint8_t> execute_instruction(opcode oc, uint8_t operand, program_state &state) {
     const auto literal_operand_value = static_cast<std::size_t>(operand);
     const auto combo_operand = combo_operand_to_value(operand, state);
 
@@ -86,7 +98,7 @@ void execute_instruction(opcode oc, uint8_t operand, program_state &state) {
         state.reg_b = state.reg_b ^ state.reg_c;
         break;
     case opcode::out:
-        std::print("{},", combo_operand.value() % 8);
+        return combo_operand.value() % 8;
         break;
     case opcode::bdv:
         state.reg_b = state.reg_a >> combo_operand.value();
@@ -95,26 +107,70 @@ void execute_instruction(opcode oc, uint8_t operand, program_state &state) {
         state.reg_c = state.reg_a >> combo_operand.value();
         break;
     }
+    return {};
 }
 
-void execute_until_halt(program_state &state) {
+std::vector<uint8_t> execute_until_halt(program_state &state) {
+    std::vector<uint8_t> output;
+
     while (true) {
         if (state.ip >= state.program.size()) { // halt program
-            std::print("\b \b\n");              // removes trailing space
-            return;
+            return output;
         }
 
         const auto oc = static_cast<opcode>(state.program[state.ip++]);
         const auto operand = state.program[state.ip++];
 
-        execute_instruction(oc, operand, state);
+        const auto out = execute_instruction(oc, operand, state);
+        if (out) {
+            output.push_back(*out);
+        }
+    }
+}
+
+bool program_reproduces_itself(const program_state &state) {
+    program_state ps = state;
+    std::size_t n_out = 0;
+
+    while (true) {
+        if (ps.ip >= ps.program.size()) {
+            return n_out == state.program.size();
+        }
+
+        const auto oc = static_cast<opcode>(ps.program[ps.ip++]);
+        const auto operand = ps.program[ps.ip++];
+
+        const auto out = execute_instruction(oc, operand, ps);
+        std::println("A = {} B = {} C = {} IP = {}", ps.reg_a, ps.reg_b, ps.reg_c, ps.ip);
+
+        if (out) {
+            if (n_out >= state.program.size() || *out != state.program[n_out]) {
+                return false;
+            }
+            ++n_out;
+        }
     }
 }
 
 int main() {
-    auto p = read_input(std::cin);
-    std::print("Part 1: ");
-    execute_until_halt(p);
+    const auto p = read_input(std::cin);
+
+    auto state_pt1 = p;
+    const auto output = execute_until_halt(state_pt1);
+    std::println("Part 1: {}", absl::StrJoin(output, ","));
+
+    std::set<program_state> not_reproducing;
+    std::size_t steps_until_reproduced = 0;
+    for (std::size_t reg_a = 0; reg_a < 100000; ++reg_a) {
+        auto state_pt2 = p;
+        state_pt2.reg_a = reg_a;
+
+        if (program_reproduces_itself(state_pt2)) {
+            steps_until_reproduced = reg_a;
+            break;
+        }
+    }
+    std::println("Part 2: {}", steps_until_reproduced);
 
     return 0;
 }
