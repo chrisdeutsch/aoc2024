8ff12d06d7c8d907c2c6f7443f3f426cff3519ac
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2024-12-23 17:41:36 +0100

    Day 19 part 2

--- src/day19.cpp
+++ src/day19.cpp
@@ -29,30 +29,28 @@ std::pair<std::vector<std::string>, std::vector<std::string>> read_input(std::is
     return {std::move(available_patterns), std::move(desired_patterns)};
 }
 
-bool is_pattern_possible(const std::string_view desired,
-                         const std::unordered_set<std::string_view> &available_patterns,
-                         const std::size_t max_pattern_length,
-                         std::unordered_map<std::string_view, bool> &mem) {
+std::size_t get_number_of_pattern_combinations(
+    const std::string_view desired, const std::unordered_set<std::string_view> &available_patterns,
+    const std::size_t max_pattern_length, std::unordered_map<std::string_view, std::size_t> &mem) {
     if (desired.size() == 0) {
-        return true;
+        return 1;
     }
 
     if (auto it = mem.find(desired); it != mem.cend()) {
         return it->second;
     }
 
+    std::size_t num_patterns = 0;
     for (std::size_t pattern_length = 1;
          pattern_length <= std::min(desired.size(), max_pattern_length); ++pattern_length) {
         const auto desired_substr = desired.substr(0, pattern_length);
-        if (available_patterns.contains(desired_substr) &&
-            is_pattern_possible(desired.substr(pattern_length), available_patterns,
-                                max_pattern_length, mem)) {
-            mem[desired] = true;
-            return true;
+        if (available_patterns.contains(desired_substr)) {
+            num_patterns += get_number_of_pattern_combinations(
+                desired.substr(pattern_length), available_patterns, max_pattern_length, mem);
         }
     }
-    mem[desired] = false;
-    return false;
+    mem[desired] = num_patterns;
+    return num_patterns;
 }
 
 int main() {
@@ -63,14 +61,20 @@ int main() {
     std::unordered_set<std::string_view> available_patterns_set(available_patterns.cbegin(),
                                                                 available_patterns.cend());
 
-    std::unordered_map<std::string_view, bool> mem;
-    std::size_t num_possible = 0;
+    std::unordered_map<std::string_view, std::size_t> mem;
+
+    std::size_t num_pattern_possible = 0;
+    std::size_t sum_pattern_combinations = 0;
     for (const auto &pattern : desired_patterns) {
-        if (is_pattern_possible(pattern, available_patterns_set, max_pattern_length, mem)) {
-            ++num_possible;
+        const auto num_comb = get_number_of_pattern_combinations(pattern, available_patterns_set,
+                                                                 max_pattern_length, mem);
+        sum_pattern_combinations += num_comb;
+        if (num_comb > 0) {
+            ++num_pattern_possible;
         }
     }
-    std::println("Part 1: {}", num_possible);
+    std::println("Part 1: {}", num_pattern_possible);
+    std::println("Part 1: {}", sum_pattern_combinations);
 
     return 0;
 }
